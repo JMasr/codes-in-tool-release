2be3bf2dea8061dbc58d974bf8979cb95820c081
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2023-01-31 13:16:18 +0100

    Update scripts

--- classifier_BLSTM_model/prepare_feats.sh
+++ classifier_BLSTM_model/prepare_feats.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Make the feats of an audio categori
-audioArray=("breathing-deep" "breathing-shallow" "cough-heavy" "cough-shallow" "counting-normal" "counting-fast" "vowel-a" "vowel-e" "vowel-o")
-for audio_cat in ${audioArray[@]}; do
-  mkdir -p feats/$audio_cat
+# Make the feats of an audio category
+for audio_cat in breathing-deep counting-normal cough-heavy cough-shallow breathing-deep breathing-shallow vowel-a vowel-e vowel-o counting-normal counting-fast; do
+  mkdir -p feats/"$audio_cat"
 done
 
 feature_config=conf/feature_config
-python local/feature_extraction.py -c $feature_config -f data/breathing-deep/dev.scp -o feats/breathing-deep &\
-python local/feature_extraction.py -c $feature_config -f data/breathing-shallow/dev.scp -o feats/breathing-shallow
-python local/feature_extraction.py -c $feature_config -f data/cough-heavy/dev.scp -o feats/cough-heavy &\
-python local/feature_extraction.py -c $feature_config -f data/cough-shallow/dev.scp -o feats/cough-shallow &\
-python local/feature_extraction.py -c $feature_config -f data/counting-normal/dev.scp -o feats/counting-normal &\
-python local/feature_extraction.py -c $feature_config -f data/counting-fast/dev.scp -o feats/counting-fast &\
-python local/feature_extraction.py -c $feature_config -f data/vowel-a/dev.scp -o feats/vowel-a &\
-python local/feature_extraction.py -c $feature_config -f data/vowel-e/dev.scp -o feats/vowel-e &\
-python local/feature_extraction.py -c $feature_config -f data/vowel-o/dev.scp -o feats/vowel-o
+for dataset in train dev val test1 test2;do
+  python local/feature_extraction.py -c $feature_config -f data/breathing-deep/$dataset.scp -o feats/breathing-deep &\
+  python local/feature_extraction.py -c $feature_config -f data/breathing-shallow/$dataset.scp -o feats/breathing-shallow
+  python local/feature_extraction.py -c $feature_config -f data/cough-heavy/$dataset.scp -o feats/cough-heavy &\
+  python local/feature_extraction.py -c $feature_config -f data/cough-shallow/$dataset.scp -o feats/cough-shallow &\
+  python local/feature_extraction.py -c $feature_config -f data/counting-normal/$dataset.scp -o feats/counting-normal &\
+  python local/feature_extraction.py -c $feature_config -f data/counting-fast/$dataset.scp -o feats/counting-fast &\
+  python local/feature_extraction.py -c $feature_config -f data/vowel-a/$dataset.scp -o feats/vowel-a &\
+  python local/feature_extraction.py -c $feature_config -f data/vowel-e/$dataset.scp -o feats/vowel-e &\
+  python local/feature_extraction.py -c $feature_config -f data/vowel-o/$dataset.scp -o feats/vowel-o
+done
