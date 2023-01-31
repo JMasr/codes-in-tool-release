035d23e72701fe24598deadf060cf377f9bcf823
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2023-01-31 13:08:03 +0100

    Update scripts

--- classifier_BLSTM_model/prepare_feats.sh
+++ classifier_BLSTM_model/prepare_feats.sh
@@ -7,12 +7,12 @@ for audio_cat in ${audioArray[@]}; do
 done
 
 feature_config=conf/feature_config
-python local/FeatureExtractor.py -c $feature_config -f data/breathing-deep/dev.scp -o feats/breathing-deep &\
-python local/FeatureExtractor.py -c $feature_config -f data/breathing-shallow/dev.scp -o feats/breathing-shallow
-python local/FeatureExtractor.py -c $feature_config -f data/cough-heavy/dev.scp -o feats/cough-heavy &\
-python local/FeatureExtractor.py -c $feature_config -f data/cough-shallow/dev.scp -o feats/cough-shallow &\
-python local/FeatureExtractor.py -c $feature_config -f data/counting-normal/dev.scp -o feats/counting-normal &\
-python local/FeatureExtractor.py -c $feature_config -f data/counting-fast/dev.scp -o feats/counting-fast &\
-python local/FeatureExtractor.py -c $feature_config -f data/vowel-a/dev.scp -o feats/vowel-a &\
-python local/FeatureExtractor.py -c $feature_config -f data/vowel-e/dev.scp -o feats/vowel-e &\
-python local/FeatureExtractor.py -c $feature_config -f data/vowel-o/dev.scp -o feats/vowel-o
+python local/feature_extraction.py -c $feature_config -f data/breathing-deep/dev.scp -o feats/breathing-deep &\
+python local/feature_extraction.py -c $feature_config -f data/breathing-shallow/dev.scp -o feats/breathing-shallow
+python local/feature_extraction.py -c $feature_config -f data/cough-heavy/dev.scp -o feats/cough-heavy &\
+python local/feature_extraction.py -c $feature_config -f data/cough-shallow/dev.scp -o feats/cough-shallow &\
+python local/feature_extraction.py -c $feature_config -f data/counting-normal/dev.scp -o feats/counting-normal &\
+python local/feature_extraction.py -c $feature_config -f data/counting-fast/dev.scp -o feats/counting-fast &\
+python local/feature_extraction.py -c $feature_config -f data/vowel-a/dev.scp -o feats/vowel-a &\
+python local/feature_extraction.py -c $feature_config -f data/vowel-e/dev.scp -o feats/vowel-e &\
+python local/feature_extraction.py -c $feature_config -f data/vowel-o/dev.scp -o feats/vowel-o
