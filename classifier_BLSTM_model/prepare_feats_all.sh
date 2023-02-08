756fb23c0a37ac76a24329ef34625c8ed49cb056
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2023-02-08 16:30:12 +0100

    Update feat pipe-line

--- classifier_BLSTM_model/prepare_feats_all.sh
+++ classifier_BLSTM_model/prepare_feats_all.sh
@@ -17,13 +17,9 @@ else
   echo "Using the default path of files: home/data/Coswara-Data/extracted_data/"
 fi
 
-echo "Make the feats of an audio categories"
-for audio_cat in cough-heavy cough-shallow breathing-deep breathing-shallow vowel-a vowel-e vowel-o counting-normal counting-fast;do
-  mkdir -p feats/$audio_cat
-done
-
-feature_config=conf/feature_config
+repo_path=$(pwd)
+feature_config=$repo_path/conf/feature_config
 for dataset in train dev test; do
   echo "Processing $dataset"
-  python local/feature_extraction.py -c $feature_config -f data_coswara_all/all/$dataset.scp -o feats_coswara_all/$dataset
+  python local/feature_extraction.py -c $feature_config -f $repo_path/data_coswara_all/all/$dataset.scp -o $repo_path/feats_coswara_all/$dataset
 done
